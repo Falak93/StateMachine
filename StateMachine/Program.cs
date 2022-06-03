122ac1719c0474907e723072bdd4affc912e749c
فلك العناني <AD0933@iu.edu.jo>
2022-06-03 18:01:47 +0300

    Add project files.

---
+++ StateMachine/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace StateMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  
+            var car=new Stateless.StateMachine<Car.State,Car.Action>(Car.State.Stopped);
+
+            car.Configure(Car.State.Stopped)
+                .Permit(Car.Action.Start, Car.State.Stopped);
+            
+            Console.WriteLine($"Stete: {car.CurrenState}");
+          
+            car.TakeAction(Car.Action.Start);
+            Console.WriteLine($"Stete: {car.CurrenState}");
+
+            car.TakeAction(Car.Action.Start);
+            Console.WriteLine($"Stete: {car.CurrenState}");
+
+            car.TakeAction(Car.Action.Accelerate);
+            Console.WriteLine($"Stete: {car.CurrenState}");
+
+            car.TakeAction(Car.Action.Stop);
+            Console.WriteLine($"Stete: {car.CurrenState}");
+        }
+    }
+}
